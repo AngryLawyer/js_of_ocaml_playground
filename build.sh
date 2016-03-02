7cf1dc726b77f92528e94ecc3310874cb769c443
Tony Aldridge <tony@dabapps.com>
2016-03-02 17:05:40 +0000

    Switched to PPX

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-ocamlbuild -use-ocamlfind -syntax camlp4o \
-    -package react,js_of_ocaml,js_of_ocaml.syntax \
+ocamlbuild -use-ocamlfind \
+    -package react,js_of_ocaml,js_of_ocaml.ppx \
     test.byte test.ml
 
 js_of_ocaml +weak.js test.byte
--- test.ml
+++ test.ml
@@ -1,14 +1,15 @@
 let print_time js_time =
-    Firebug.console##log (js_time##toTimeString ())
+    Firebug.console##log (js_time##toTimeString)
 
 open React;;
 
 let seconds, run =
     let e, send = E.create () in
     let run () = begin
-        ignore Dom_html.window##setInterval (Js.wrap_callback (fun () -> send (
-            jsnew Js.date_now ()
-        )), 1000.0)
+        ignore (Dom_html.window##setInterval (Js.wrap_callback (fun () ->
+            let now = new%js Js.date_now in
+            send now
+        )) 1000.0)
     end in
     e, run
 
