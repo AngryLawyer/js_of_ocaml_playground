71ea68c39b16c8b02c46a70f026bef74a0647aa2
Tony Aldridge <tony@dabapps.com>
2016-03-02 17:09:49 +0000

    Open commands are evil

--- test.ml
+++ test.ml
@@ -1,10 +1,8 @@
 let print_time js_time =
     Firebug.console##log (js_time##toTimeString)
 
-open React;;
-
 let seconds, run =
-    let e, send = E.create () in
+    let e, send = React.E.create () in
     let run () = begin
         ignore (Dom_html.window##setInterval (Js.wrap_callback (fun () ->
             let now = new%js Js.date_now in
@@ -13,6 +11,6 @@ let seconds, run =
     end in
     e, run
 
-let printer = E.map print_time seconds
+let printer = React.E.map print_time seconds
 
 let () = run ()
