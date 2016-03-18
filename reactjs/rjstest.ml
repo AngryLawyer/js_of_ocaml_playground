085d4faed78eb3c205463b3a64e1b7071a2050b5
Tony Aldridge <tony@dabapps.com>
2016-03-18 17:07:34 +0000

    Trying to render basic h1 item

--- reactjs/rjstest.ml
+++ reactjs/rjstest.ml
@@ -1,9 +1,14 @@
 module ReactJS = struct
-    let ReactJS = (Js.unsafe.variable "React")
-    let ReactDOM = (Js.unsafe.variable "ReactDOM")
+    let reactJs = (Js.Unsafe.variable "React")
+    let reactDom = (Js.Unsafe.variable "ReactDOM")
 
-    let createElement = string dict ~content =
-        ()
+    let createElement string ~content =
+        Js.Unsafe.meth_call reactJs "createElement" [| Js.Unsafe.inject (Js.string string); Js.Unsafe.inject Js.null |]
+
+    let render item target =
+        Js.Unsafe.meth_call reactDom "render" [| Js.Unsafe.inject item; Js.Unsafe.inject target |]
 end
 
-let () = ()
+let () =
+    let target = Dom_html.getElementById "content" in
+    ReactJS.render (ReactJS.createElement "h1") target
