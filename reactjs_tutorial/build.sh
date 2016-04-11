21660b1e289b03e904476abd829d7f6faf1be7b9
Tony Aldridge <tony@dabapps.com>
2016-04-11 14:27:55 +0100

    Try to build in correct order

--- reactjs_tutorial/build.sh
+++ reactjs_tutorial/build.sh
@@ -1,6 +1,5 @@
 ocamlbuild -use-ocamlfind \
-    -package reactjs,reactjs_ppx \
-    -package react,js_of_ocaml,js_of_ocaml.ppx \
+    -package reactjs,reactjs_ppx,react,js_of_ocaml,js_of_ocaml.ppx \
     tutorial.byte tutorial.ml
 
 js_of_ocaml +weak.js tutorial.byte
