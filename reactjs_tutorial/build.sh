250c3d9b98830d5d19a0a9ae548859b2a784c2b3
Tony Aldridge <zaragopha@hotmail.com>
2016-05-16 21:50:04 +0100

    Updated ReactJS_of_ocaml usage

--- reactjs_tutorial/build.sh
+++ reactjs_tutorial/build.sh
@@ -1,6 +1,8 @@
 ocamlbuild -use-ocamlfind \
-    -package reactjs,reactjs_ppx,js_of_ocaml,js_of_ocaml.ppx,omd \
+    -package reactjs,reactjs_ppx,js_of_ocaml,js_of_ocaml.ppx,omd,commonjs_ppx \
     tutorial.byte tutorial.ml
 
 js_of_ocaml +weak.js tutorial.byte
+./node_modules/.bin/browserify tutorial.js > main.js
 rm tutorial.byte
+rm tutorial.js
--- reactjs_tutorial/tutorial.ml
+++ reactjs_tutorial/tutorial.ml
@@ -23,23 +23,25 @@ let data = begin
     log data
 end
 
-let comment = ReactJS.create_class (object%js (self)
+module Comment = ReactJS.Make_ReactClass(struct
+    type props_spec = < author: string Js.readonly_prop > Js.t
+    type state_spec = < > Js.t
+end)
+let comment = Comment.create_class (object%js (self)
     method render =
-        let props = ReactJS.get_props self in
+        let props = Comment.get_props self in
         [%jsx [div; className "comment"; [
             [h2; className "commentAuthor"; [
-                [%code ReactJS.Dom_string (default (ReactJS.get_prop props "author") "NO AUTHOR")];
-            ]];
-            [%code match ReactJS.get_prop props "children" with
-                | Some element -> let stringy_element = to_string element in
-                    let markup = raw_markup stringy_element in
-                    ReactJS.React_element [%jsx [span; dangerouslySetInnerHTML markup]]
-                | None -> ReactJS.No_content
-            ]
+                [%code ReactJS.Dom_string (props##.author)];
+            ]]
         ]]]
 end)
 
-let comment_list = ReactJS.create_class (object%js
+module Comment_list = ReactJS.Make_ReactClass(struct
+    type props_spec = < > Js.t
+    type state_spec = < > Js.t
+end)
+let comment_list = Comment_list.create_class (object%js
     method render =
         [%jsx [div; className "commentList"; [
             [Comment; author "Pete Hunt"; ["This is one comment"]];
@@ -47,14 +49,23 @@ let comment_list = ReactJS.create_class (object%js
         ]]]
 end)
 
-let comment_form = ReactJS.create_class (object%js
+module Comment_form = ReactJS.Make_ReactClass(struct
+    type props_spec = < > Js.t
+    type state_spec = < > Js.t
+end)
+let comment_form = Comment_form.create_class (object%js
     method render =
         [%jsx [div; className "commentForm"; [
             "Hello world! I am a CommentForm!"
         ]]]
 end)
 
-let comment_box = ReactJS.create_class (object%js
+
+module Comment_box = ReactJS.Make_ReactClass(struct
+    type props_spec = < > Js.t
+    type state_spec = < > Js.t
+end)
+let comment_box = Comment_box.create_class (object%js
    method render =
        [%jsx [div; className "commentBox"; [
            [h1; ["Comments"]];
